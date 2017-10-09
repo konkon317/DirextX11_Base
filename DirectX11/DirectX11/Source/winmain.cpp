875e889c24c5eb4ee85689a34d8dc7bb4a002fa0
KousukeFujii_Win7_DeskTopPC <konkon.317@gmail.com>
2017-10-10 00:09:12 +0900

    追加　プロジェクトファイル　とりあえず最低限コンパイルが通るコード
    branch : master

---
+++ DirectX11/DirectX11/Source/winmain.cpp
@@ -0,0 +1,26 @@
+﻿#if _DEBUG
+
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
+#endif
+
+#include <Windows.h>
+#pragma comment(lib, "winmm.lib")
+
+
+
+
+//他のインクルードはこの上に追加すること
+#if _DEBUG
+#define new ::new(_NORMAL_BLOCK,__FILE__,__LINE__)
+#endif
+
+
+int _stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+	return 0;
+}
